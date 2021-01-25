a6de72eb4b5c3c5dea8ab44412b3829781860fba
Tommy <thomasvisvader@gmail.com>
2021-01-25 14:15:37 +0100

    Coleco Telstar Tennis done

--- Coleco Telstar/Coleco Telstar.py
+++ Coleco Telstar/Coleco Telstar.py
@@ -9,29 +9,31 @@ from pygame.locals import *
 
 
 def left_score():
-    global left_points, ballMoving, ball_speed, dirchoice, scored
+    global left_points, ballMoving, ball_speed, dirchoice, scored, leftMoving, ballSpawning
     left_points += 1
     ballMoving = False
-    ball_speed = 10.0
     ball.center = (width + 50, 300)
     dirchoice = 1
     scored = True
+    leftMoving = False
+    ballSpawning = True
     score_sound.play()
 
 
 def right_score():
-    global right_points, ballMoving, ball_speed, dirchoice, scored
+    global right_points, ballMoving, ball_speed, dirchoice, scored, leftMoving, ballSpawning
     right_points += 1
     ballMoving = False
-    ball_speed = 10.0
     ball.center = (-50, 300)
     dirchoice = 0
     score_sound.play()
+    leftMoving = True
+    ballSpawning = True
     scored = True
 
 
 def paddle_collision(ball, paddle):
-    global vx, vy, ball_speed, direction, leftMoving
+    global vx, vy, ball_speed, direction, leftMoving, speedup
     alpha = 50
     c_point = ball.center[1] - paddle.center[1]
     c_point = c_point / (paddle_height / 2)
@@ -60,8 +62,6 @@ def paddle_collision(ball, paddle):
             direction = 180 - angle
         leftMoving = True
         pygame.mouse.set_pos([width / 2, left_paddle.y])
-    if ball_speed < 22.0:
-        ball_speed += 1
     if 181 <= direction <= 185:
         direction = 180
     elif 355 <= direction <= 359:
@@ -70,25 +70,27 @@ def paddle_collision(ball, paddle):
     vy = ball_speed * math.sin(math.radians(direction))
     ball.x += vx
     ball.y += vy
-    hit_sound.play()
+    paddle_hit_sound.play()
     return ball
 
 
 def ball_movement(ball):
-    global vx, vy
+    global vx, vy, ballSpawning
     if ball.right >= width:
         left_score()
     elif ball.left <= 0:
         right_score()
+    elif ballSpawning and 700 <= ball.centerx <= 900:
+        ballSpawning = False
     else:
         if ball.top <= 12:
             ball.top = 12
             vy *= -1
-            hit_sound.play()
+            wall_hit_sound.play()
         elif ball.bottom >= height - 12:
             ball.bottom = height - 12
             vy *= -1
-            hit_sound.play()
+            wall_hit_sound.play()
         ball.x += vx
         ball.y += vy
     return ball
@@ -109,21 +111,6 @@ def move_down(paddle):
     paddle.y += paddle_speed
     return paddle
 
-
-# def draw_net():
-#     red, green, blue = hsl_to_rgb(192, 0.95, 0.5)
-#     for i in range(15):
-#         pygame.draw.rect(screen, (red, green, blue), (623, i * 68, 3, 34))
-#     pixel = 0
-#     for h in range(193, 199, 2):
-#         red, green, blue = hsl_to_rgb(h, 0.95, 0.5)
-#         for i in range(15):
-#             pygame.draw.rect(screen, (red, green, blue), (625 + pixel, i * 68, 4, 34))
-#         pixel += 4
-#         red, green, blue = hsl_to_rgb(h + 1, 0.95, 0.5)
-#         for i in range(15):
-#             pygame.draw.rect(screen, (red, green, blue), (625 + pixel, i * 68, 5, 34))
-#         pixel += 5
 
 def draw_walls():
     pygame.draw.rect(screen, (255, 255, 255), (0, 0, 20, 12))
@@ -167,18 +154,25 @@ def write_score():
 def new_game(type):
     screen.fill((0, 0, 0))
     global left_points, right_points, leftMovingUp, leftMovingDown, rightMovingUp, rightMovingDown, \
-        gameStarted, ball, ballTimer, ballMoving
+        gameStarted, ball, ballTimer, ballMoving, dirchoice, leftMoving
     if type == 0:
         gameStarted = False
-    ball.center = (ballx, bally)
-    left_points = 0
-    right_points = 0
-    ballTimer = 0
-    leftMovingUp = False
-    leftMovingDown = False
-    rightMovingUp = False
-    rightMovingDown = False
-    ballMoving = False
+    else:
+        dirchoice = random.choice([0, 1])
+        if dirchoice == 0:
+            leftMoving = True
+        else:
+            leftMoving = False
+        ball.center = (ballx, bally)
+        left_points = 0
+        right_points = 0
+        ballTimer = 0
+        leftMovingUp = False
+        leftMovingDown = False
+        rightMovingUp = False
+        rightMovingDown = False
+        ballMoving = False
+        gameStarted = True
 
 
 pygame.init()
@@ -189,9 +183,9 @@ screen = pygame.display.set_mode((width, height))
 score_screen = pygame.Surface((width, 255))
 pygame.display.set_caption('Coleco Telstar')
 scoreFont = pygame.freetype.Font('text/colecoscore.ttf', 105)
+wall_hit_sound = pygame.mixer.Sound('sounds/wallhit.wav')
 score_sound = pygame.mixer.Sound('sounds/score.wav')
-hit_sound = pygame.mixer.Sound('sounds/hit.wav')
-spawn_sound = pygame.mixer.Sound('sounds/spawn.wav')
+paddle_hit_sound = pygame.mixer.Sound('sounds/paddlehit.wav')
 
 FPS = 60
 fpsClock = pygame.time.Clock()
@@ -204,18 +198,18 @@ leftx = 32
 lefty = height // 2
 rightx = width - 32 - paddle_width
 righty = height // 2
-ballx = width // 2 - 5
-bally = height // 2 + 5
+ballx = -50
+bally = -50
 paddle_speed = 10.0
-ball_speed = 10.0
+ball_speed = 15.0
 
 topWall = pygame.Rect(0, 0, width, 12)
 bottomWall = pygame.Rect(0, height-12, width, 12)
 net = pygame.Rect(633, 0, paddle_width, height)
 ball = pygame.Rect(ballx, bally, ball_width, ball_height)
-ball.center = (ballx, bally)
 left_paddle = pygame.Rect(leftx, lefty, paddle_width, paddle_height)
 right_paddle = pygame.Rect(rightx, righty, paddle_width, paddle_height)
+
 
 left_points = 0
 right_points = 0
@@ -228,6 +222,8 @@ rightMovingUp = False
 rightMovingDown = False
 ballMoving = False
 scored = False
+gameStarted = True
+ballSpawning = False
 
 make_score()
 dirchoice = random.choice([0, 1])
@@ -257,6 +253,21 @@ while True:
         elif event.type == KEYUP and event.key == K_ESCAPE:
             pygame.quit()
             sys.exit()
+        elif event.type == KEYUP and event.key == K_1:
+            ball_speed = 15.0
+            paddle_height = 150
+            left_paddle = pygame.Rect(leftx, pygame.mouse.get_pos()[1], paddle_width, paddle_height)
+            right_paddle = pygame.Rect(rightx, pygame.mouse.get_pos()[1], paddle_width, paddle_height)
+        elif event.type == KEYUP and event.key == K_2:
+            ball_speed = 15.0
+            paddle_height = 75
+            left_paddle = pygame.Rect(leftx, pygame.mouse.get_pos()[1], paddle_width, paddle_height)
+            right_paddle = pygame.Rect(rightx, pygame.mouse.get_pos()[1], paddle_width, paddle_height)
+        elif event.type == KEYUP and event.key == K_3:
+            ball_speed = 30.0
+            paddle_height = 150
+            left_paddle = pygame.Rect(leftx, pygame.mouse.get_pos()[1], paddle_width, paddle_height)
+            right_paddle = pygame.Rect(rightx, pygame.mouse.get_pos()[1], paddle_width, paddle_height)
         if controls == 'Mouse':
             if event.type == MOUSEMOTION:
                 position = pygame.mouse.get_pos()
@@ -284,17 +295,18 @@ while True:
                     rightMovingUp = True
                 elif event.key == K_DOWN:
                     rightMovingDown = True
-    if not ballMoving and ballTimer >= 2:
+    if not ballMoving and ballTimer >= 1:
         ballMoving = True
-        ball.x = ballx
         ball.y = random.randint(66, height-84)
         if dirchoice == 0:
+            ball.x = width - 35
             quadrant = random.choice([2, 3])
             if quadrant == 2:
                 direction = random.randint(91, 180)
             else:
                 direction = random.randint(186, 269)
         else:
+            ball.x = 35
             quadrant = random.choice([1, 4])
             if quadrant == 1:
                 direction = random.randint(0, 84)
@@ -302,7 +314,6 @@ while True:
                 direction = random.randint(276, 354)
         vx = ball_speed * math.cos(math.radians(direction))
         vy = ball_speed * math.sin(math.radians(direction))
-        spawn_sound.play()
         ballTimer = 0
     if ballMoving:
         ball = ball_movement(ball)
@@ -315,20 +326,21 @@ while True:
             right_paddle = move_up(right_paddle)
         elif rightMovingDown and not rightMovingUp:
             right_paddle = move_down(right_paddle)
-    if ball.colliderect(left_paddle):
+    if gameStarted and not ballSpawning and ball.colliderect(left_paddle):
         ball = paddle_collision(ball, left_paddle)
-    elif ball.colliderect(right_paddle):
+    elif gameStarted and not ballSpawning and ball.colliderect(right_paddle):
         ball = paddle_collision(ball, right_paddle)
     if scored:
         scored = False
-        make_score()
+        if gameStarted:
+            make_score()
         scoreTime = time.time()
         if left_points == 15 or right_points == 15:
             new_game(0)
     if not ballMoving:
-        write_score()
         ballTimer = time.time() - scoreTime
     draw_walls()
+    write_score()
     pygame.draw.rect(screen, (255, 255, 255), net)
     pygame.draw.rect(screen, (255, 255, 255), left_paddle)
     pygame.draw.rect(screen, (255, 255, 255), right_paddle)
