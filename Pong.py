4d396acd7a42fb7e286be9dbe0dc6206eea1aaa2
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-12-07 13:59:12 +0000

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -4,77 +4,55 @@ import random
 import math
 from pygame.locals import *
 
-
 def start():
     angle = random.randrange(0, 360)
-    dir = math.radians(angle)
-    return dir
-
+    return math.radians(angle)
 
 def left_score():
     global left_points, ballx, bally, ballMoving
     left_points += 1
-    ballx = 758
-    bally = 388
+    ballx = width // 2
+    bally = height // 2
     ballMoving = False
-
 
 def right_score():
     global right_points, ballx, bally, ballMoving
     right_points += 1
-    ballx = 758
-    bally = 388
+    ballx = width // 2
+    bally = height // 2
     ballMoving = False
 
-
 def ball_collision(direction):
-    direction += math.radians(90)
-    return direction
+    return direction + math.pi
 
-
-def ball_movement(direction, collision):
+def ball_movement(direction):
     global ballx, bally
-    if collision or bally >= height-ballsize or bally <= 0:
+    if bally >= height - ballsize or bally <= 0:
         direction = ball_collision(direction)
-        ballx = ballx + (5 * math.sin(direction))
-        bally = bally - (5 * math.cos(direction))
-    elif ballx >= width-ballsize:
+    elif ballx >= width - ballsize:
         left_score()
     elif ballx <= 0:
         right_score()
-    else:
-        ballx = ballx + (5 * math.sin(direction))
-        bally = bally - (5 * math.cos(direction))
-    return direction
 
+    ballx += 5 * math.sin(direction)
+    bally -= 5 * math.cos(direction)
+    return direction
 
 def left_move_up():
     global lefty
-    if lefty <= 0:
-        return
-    lefty -= 5
-
+    lefty = max(0, lefty - 5)
 
 def left_move_down():
     global lefty
-    if lefty >= 740:
-        return
-    lefty += 5
-
+    lefty = min(height - paddle_height, lefty + 5)
 
 def right_move_up():
     global righty
-    if righty <= 0:
-        return
-    righty -= 5
-
+    righty = max(0, righty - 5)
 
 def right_move_down():
     global righty
-    if righty >= 740:
-        return
-    righty += 5
-
+    righty = min(height - paddle_height, righty + 5)
 
 pygame.init()
 height = 795
@@ -93,11 +71,11 @@ paddle_height = 55
 paddle_width = 41
 ballsize = 19
 leftx = 140
-lefty = 370
-rightx = 1354
-righty = 370
-ballx = 758
-bally = 388
+lefty = height // 2 - paddle_height // 2
+rightx = width - 140 - paddle_width
+righty = height // 2 - paddle_height // 2
+ballx = width // 2
+bally = height // 2
 
 left_points = 0
 right_points = 0
@@ -107,12 +85,13 @@ leftMovingDown = False
 rightMovingUp = False
 rightMovingDown = False
 ballMoving = False
+direction = 0
 
 while True:
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
-            print('Left: {}, Right: {}'.format(left_points, right_points))
+            print(f'Left: {left_points}, Right: {right_points}')
             pygame.quit()
             sys.exit()
         elif event.type == KEYUP:
@@ -137,8 +116,9 @@ while True:
                 if not ballMoving:
                     ballMoving = True
                     direction = start()
+
     if ballMoving:
-        direction = ball_movement(direction, 0)
+        direction = ball_movement(direction)
     if leftMovingUp:
         left_move_up()
     elif leftMovingDown:
@@ -147,9 +127,11 @@ while True:
         right_move_up()
     elif rightMovingDown:
         right_move_down()
-    if leftx < ballx <= leftx+paddle_width and lefty < bally <= lefty+paddle_height or \
-            rightx < ballx <= rightx+paddle_width and righty < bally <= righty+paddle_height:
-        direction = ball_movement(direction, 1)
+
+    if (leftx < ballx <= leftx + paddle_width and lefty < bally <= lefty + paddle_height) or \
+       (rightx < ballx <= rightx + paddle_width and righty < bally <= righty + paddle_height):
+        direction = ball_collision(direction)
+
     screen.blit(ball, (ballx, bally))
     screen.blit(leftpaddle, (leftx, lefty))
     screen.blit(rightpaddle, (rightx, righty))
